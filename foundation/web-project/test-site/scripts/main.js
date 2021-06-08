95c2c874af4da22d032e44b02f051842757ffc9f
Abel Ortiz <ortizharoabel@gmail.com>
2021-06-07 21:38:56 -0500

    main.js variables con let for declare variable many times

--- foundation/web-project/test-site/scripts/main.js
+++ foundation/web-project/test-site/scripts/main.js
@@ -1,2 +1,10 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
+
+let myvariable = 'Bob';
+myvariable = 'Abel';
+myvariable = ['Abel', 25, 'Carlos', true, 'hola']
+
+myvariable[2]
+
+/* This is a comment like Css */ or // this comment cotains no line breaks
